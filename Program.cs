c84272ae5be6034399c90b6cefaeeb9a228870db
Ian McConihay <mcconihay.ian92@gmail.com>
2022-03-09 15:35:56 -0600

    First

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Put in a name");
+string? input = Console.ReadLine();
+
+Console.WriteLine("How many times do you want to repeat the name");
+string? times = Console.ReadLine();
+
+for(int i=0; i< Convert.ToInt32(times); i++){
+    Console.WriteLine("Hello "+ input);
+}
