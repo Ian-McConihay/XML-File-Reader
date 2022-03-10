d261c9fa2bd252ab96d86b65a1df871f8ac3f4d9
Ian McConihay <mcconihay.ian92@gmail.com>
2022-03-10 09:21:31 -0600

    Trying to read

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+// Console.WriteLine();
+// Console.WriteLine("Input a name");
+// string? input = Console.ReadLine();
 
-Console.WriteLine("Put in a name");
-string? input = Console.ReadLine();
+// Console.WriteLine("How many times do you want to repeat the name");
+// string? times = Console.ReadLine();
 
-Console.WriteLine("How many times do you want to repeat the name");
-string? times = Console.ReadLine();
+// for(int i=0; i< Convert.ToInt32(times); i++){
+//     Console.WriteLine("Hello "+ input);
+// }
+using System;
+using System.Xml;
 
-for(int i=0; i< Convert.ToInt32(times); i++){
-    Console.WriteLine("Hello "+ input);
+namespace XMLfileReader{
+    class Program{
+        static void Main(string[] args){
+            XmlTextReader xtr = new XmlTextReader("/Users/ianmcconihay/XMLfileReader/testcase.xml");
+            while (xtr.Read()){
+                if (xtr.NodeType == XmlNodeType.Element && xtr.Name == "name"){
+                    string s1 = xtr.ReadElementString();
+                    Console.WriteLine("Name = " + s1);
+                }
+            }
+
+
+        }
+    }
 }
