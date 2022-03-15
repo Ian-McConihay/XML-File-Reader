7bf033d8b49022621784f2aa82a85731153d7a14
Ian McConihay <mcconihay.ian92@gmail.com>
2022-03-14 22:04:20 -0500

    Im almost there

--- Program.cs
+++ Program.cs
@@ -13,90 +13,156 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Xml;
-using static XMLfileReader.obj.Record;
+using System.Xml.Linq;
+// using static XMLfileReader.obj.Record;
 
 namespace XMLfileReader{
     
-    class Program {
-        static void Main(string[] args){
+    public class Program {
+       
+
             
-            IList<Record> records = new List<Record>();
             
-            string path = Path.Combine(Environment.CurrentDirectory, "testcase.xml");
+            // IList<Record> records = new List<Record>();
+            
+            // string path = Path.Combine(Environment.CurrentDirectory, "testcase.xml");
 
-            XmlSerializer xs = new XmlSerializer(typeof(List<Record>));
+            // XmlSerializer xs = new XmlSerializer(typeof(List<Record>));
 
-            using(FileStream stream = File.Open(path, FileMode.Open)){
-                records = (List<Record>)xs.Deserialize(stream);
-            }
+            // using(FileStream stream = File.Open(path, FileMode.Open)){
+            //     records = (List<Record>)xs.Deserialize(stream);
+            // }
+            
 
-
-
+            
 
             // string sourceFolderPath ="D:\Kaushik\AllFiles"  
   
-            if (Directory.Exists(path))  
-            {  
-                DirectoryInfo dirSource = new DirectoryInfo(path);  
-                var allXMLFiles = dirSource.GetFiles("*.xml", SearchOption.AllDirectories).ToList();  
+            // if (Directory.Exists(path))  {  
+            //     DirectoryInfo dirSource = new DirectoryInfo(path);  
+            //     var allXMLFiles = dirSource.GetFiles("*.xml", SearchOption.AllDirectories).ToList();  
             
-                List<Record> listAllRecordss = new List<Record>();  
+            //     List<Record> listAllRecordss = new List<Record>();  
             
-                foreach (var nextFile in allXMLFiles)  
-                {  
-                    try  
-                    {  
-                        XmlSerializer serializer = new XmlSerializer(typeof(Record));  
-                        using (TextReader reader = new StringReader(System.IO.File.ReadAllText(nextFile.FullName)))  
-                        {  
-                            Record result = (Record)serializer.Deserialize(reader);  
-                            listAllRecordss.Add(result);  
-                        }  
-                    }  
-                    catch (Exception ex)  
-                    {  
+            //     foreach (var nextFile in allXMLFiles)  
+            //     {  
+            //         try  
+            //         {  
+            //             XmlSerializer serializer = new XmlSerializer(typeof(Record));  
+            //             using (TextReader reader = new StringReader(System.IO.File.ReadAllText(nextFile.FullName)))  
+            //             {  
+            //                 Record result = (Record)serializer.Deserialize(reader);  
+            //                 listAllRecordss.Add(result);  
+            //             }  
+            //         }  
+            //         catch (Exception ex)  
+            //         {  
             
-                    }  
-                }  
-            } 
+            //         }  
+            //     }  
+            // } 
 
         
 
 
+      public class Record {
+        [XmlAttribute(AttributeName="Debtor")]
+        public string Debtor { get; set; }
+        [XmlAttribute(AttributeName="DebtorSSN")]
+        public long DebtorSSN { get; set; }
+        [XmlAttribute(AttributeName="DebtorAddress1")]
+        public string DebtorAddress1 { get; set; }
+        [XmlAttribute(AttributeName="DebtorAddress2")]
+        public string DebtorAddress2 { get; set; }
+        [XmlAttribute(AttributeName="DebtorAddress3")]
+        public string DebtorAddress3 { get; set; }
+        [XmlAttribute(AttributeName="DebtorCounty")]
+        public string DebtorCounty { get; set; }
+        [XmlAttribute(AttributeName="DebtorEMail")]
+        public string DebtorEMail { get; set; }
+        [XmlAttribute(AttributeName="DebtorDateofBirth")]
+        public string DebtorDateofBirth { get; set; }
+        [XmlAttribute(AttributeName="DebtorCellPhone")]
+        public string DebtorCellPhone { get; set; }
+        [XmlAttribute(AttributeName="Spouse")]
+        public string Spouse { get; set; }
+        [XmlAttribute(AttributeName="SpouseSSN")]
+        public string SpouseSSN { get; set; }
+        [XmlAttribute(AttributeName="SpouseAddress1")]
+        public string SpouseAddress1 { get; set; }
+        [XmlAttribute(AttributeName="SpouseAddress2")]
+        public string SpouseAddress2 { get; set; }
+        [XmlAttribute(AttributeName="SpouseAddress3")]
+        public string SpouseAddress3 { get; set; }
+        [XmlAttribute(AttributeName="DebtorCounty")]
+        public string DebtorCounty { get; set; }
+        [XmlAttribute(AttributeName="SpouseEMail")]
+        public string SpouseEMail { get; set; }
+        [XmlAttribute(AttributeName="SpouseDateofBirth")]
+        public string SpouseDateofBirth { get; set; }
+        [XmlAttribute(AttributeName="SpouseCellPhone")]
+        public string SpouseCellPhone { get; set; }
+        [XmlAttribute(AttributeName="Chapter")]
+        public string Chapter { get; set; }
+        [XmlAttribute(AttributeName="TypeofDebtor")]
+        public string TypeofDebtor { get; set; }
+        [XmlAttribute(AttributeName="AttorneyBarId")]
+        public string AttorneyBarId { get; set; }
+        [XmlAttribute(AttributeName="IsActive")]
+        public string IsActive { get; set; }
 
-   Test t = new Test();
+    }
+   Progam t = new Program();
+    static void Main(string[] args){
         // Read a purchase order.
-        t.DeserializeObject("simple.xml");
+        t.DeserializeObject("testcase.xml");
     
 
      void DeserializeObject(string filename){
         Console.WriteLine("Reading with Stream");
         // Create an instance of the XmlSerializer.
         XmlSerializer serializer =
-        new XmlSerializer(typeof(OrderedItem));
+        new XmlSerializer(typeof(Record));
 
         // Declare an object variable of the type to be deserialized.
-        OrderedItem i;
+        Record i;
 
         using (Stream reader = new FileStream(filename, FileMode.Open)){
             // Call the Deserialize method to restore the object's state.
-            i = (OrderedItem)serializer.Deserialize(reader);
+            i = (Record)serializer.Deserialize(reader);
         }
 
         // Write out the properties of the object.
         Console.Write(
-        i.ItemName + "\t" +
-        i.Description + "\t" +
-        i.UnitPrice + "\t" +
-        i.Quantity + "\t" +
-        i.LineTotal);
+        i.Debtor + "\t" +
+        i.DebtorSSN + "\t" +
+        i.DebtorAddress1 + "\t" +
+        i.DebtorAddress2 + "\t" +
+        i.DebtorAddress3 + "\t" +
+        i.DebtorCounty + "\t" +
+        i.DebtorEMail + "\t" +
+        i.DebtorDateofBirth + "\t" +
+        i.DebtorCellPhone + "\t" +
+        i.Spouse + "\t" +
+        i.SpouseSSN + "\t" +
+        i.SpouseAddress1 + "\t" +
+        i.SpouseAddress2 + "\t" +
+        i.SpouseAddress3 + "\t" +
+        i.DebtorCounty + "\t" +
+        i.SpouseEMail + "\t" +
+        i.SpouseDateofBirth + "\t" +
+        i.SpouseCellPhone + "\t" +
+        i.Chapter + "\t" +
+        i.TypeofDebtor + "\t" +
+        i.AttorneyBarId + "\t" +
+        i.IsActive);
     
 
 
         
-
-
     }
+
+        
         }
     }
 }
--- obj/Record.cs
+++ obj/Record.cs
@@ -1,64 +1,64 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Xml.Serialization;
+// using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Xml.Serialization;
 
 
 
-namespace XMLfileReader.obj  {
-    [XmlRoot(ElementName="record")]
-    public class Record {
-        [XmlAttribute(AttributeName="Debtor")]
-        public string Debtor { get; set; }
-        [XmlAttribute(AttributeName="DebtorSSN")]
-        public long DebtorSSN { get; set; }
-        [XmlAttribute(AttributeName="DebtorAddress1")]
-        public string DebtorAddress1 { get; set; }
-        [XmlAttribute(AttributeName="DebtorAddress2")]
-        public string DebtorAddress2 { get; set; }
-        [XmlAttribute(AttributeName="DebtorAddress3")]
-        public string DebtorAddress3 { get; set; }
-        [XmlAttribute(AttributeName="DebtorCounty")]
-        public string DebtorCounty { get; set; }
-        [XmlAttribute(AttributeName="DebtorEMail")]
-        public string DebtorEMail { get; set; }
-        [XmlAttribute(AttributeName="DebtorDateofBirth")]
-        public string DebtorDateofBirth { get; set; }
-        [XmlAttribute(AttributeName="DebtorCellPhone")]
-        public string DebtorCellPhone { get; set; }
-        [XmlAttribute(AttributeName="Spouse")]
-        public string Spouse { get; set; }
-        [XmlAttribute(AttributeName="SpouseSSN")]
-        public string SpouseSSN { get; set; }
-        [XmlAttribute(AttributeName="SpouseAddress1")]
-        public string SpouseAddress1 { get; set; }
-        [XmlAttribute(AttributeName="SpouseAddress2")]
-        public string SpouseAddress2 { get; set; }
-        [XmlAttribute(AttributeName="SpouseAddress3")]
-        public string SpouseAddress3 { get; set; }
-        [XmlAttribute(AttributeName="DebtorCounty")]
-        public string DebtorCounty { get; set; }
-        [XmlAttribute(AttributeName="SpouseEMail")]
-        public string SpouseEMail { get; set; }
-        [XmlAttribute(AttributeName="SpouseDateofBirth")]
-        public string SpouseDateofBirth { get; set; }
-        [XmlAttribute(AttributeName="SpouseCellPhone")]
-        public string SpouseCellPhone { get; set; }
-        [XmlAttribute(AttributeName="Chapter")]
-        public string Chapter { get; set; }
-        [XmlAttribute(AttributeName="TypeofDebtor")]
-        public string TypeofDebtor { get; set; }
-        [XmlAttribute(AttributeName="AttorneyBarId")]
-        public string AttorneyBarId { get; set; }
-        [XmlAttribute(AttributeName="IsActive")]
-        public string IsActive { get; set; }
+// namespace XMLfileReader.obj  {
+//     [XmlRoot(ElementName="record")]
+//     public class Record {
+//         [XmlAttribute(AttributeName="Debtor")]
+//         public string Debtor { get; set; }
+//         [XmlAttribute(AttributeName="DebtorSSN")]
+//         public long DebtorSSN { get; set; }
+//         [XmlAttribute(AttributeName="DebtorAddress1")]
+//         public string DebtorAddress1 { get; set; }
+//         [XmlAttribute(AttributeName="DebtorAddress2")]
+//         public string DebtorAddress2 { get; set; }
+//         [XmlAttribute(AttributeName="DebtorAddress3")]
+//         public string DebtorAddress3 { get; set; }
+//         [XmlAttribute(AttributeName="DebtorCounty")]
+//         public string DebtorCounty { get; set; }
+//         [XmlAttribute(AttributeName="DebtorEMail")]
+//         public string DebtorEMail { get; set; }
+//         [XmlAttribute(AttributeName="DebtorDateofBirth")]
+//         public string DebtorDateofBirth { get; set; }
+//         [XmlAttribute(AttributeName="DebtorCellPhone")]
+//         public string DebtorCellPhone { get; set; }
+//         [XmlAttribute(AttributeName="Spouse")]
+//         public string Spouse { get; set; }
+//         [XmlAttribute(AttributeName="SpouseSSN")]
+//         public string SpouseSSN { get; set; }
+//         [XmlAttribute(AttributeName="SpouseAddress1")]
+//         public string SpouseAddress1 { get; set; }
+//         [XmlAttribute(AttributeName="SpouseAddress2")]
+//         public string SpouseAddress2 { get; set; }
+//         [XmlAttribute(AttributeName="SpouseAddress3")]
+//         public string SpouseAddress3 { get; set; }
+//         [XmlAttribute(AttributeName="DebtorCounty")]
+//         public string DebtorCounty { get; set; }
+//         [XmlAttribute(AttributeName="SpouseEMail")]
+//         public string SpouseEMail { get; set; }
+//         [XmlAttribute(AttributeName="SpouseDateofBirth")]
+//         public string SpouseDateofBirth { get; set; }
+//         [XmlAttribute(AttributeName="SpouseCellPhone")]
+//         public string SpouseCellPhone { get; set; }
+//         [XmlAttribute(AttributeName="Chapter")]
+//         public string Chapter { get; set; }
+//         [XmlAttribute(AttributeName="TypeofDebtor")]
+//         public string TypeofDebtor { get; set; }
+//         [XmlAttribute(AttributeName="AttorneyBarId")]
+//         public string AttorneyBarId { get; set; }
+//         [XmlAttribute(AttributeName="IsActive")]
+//         public string IsActive { get; set; }
 
-    }
-    public IList<Record> getAllRecords(){
-        return new List<Record>(){
+//     }
+//     public IList<Record> getAllRecords(){
+//         return new List<Record>(){
 
-        };
-    }
+//         };
+//     }
 
 
 
